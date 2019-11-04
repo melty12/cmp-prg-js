f2cf238d54be64af45241918cdaf0e000bfec840
melty12 <komei.1220@gmail.com>
2019-11-04 12:16:59 +0900

    update: stop using Set() and optimize loop processing

--- 81/main.js
+++ 81/main.js
@@ -2,19 +2,15 @@ function Main(input)
 {
 	input = parseInt(input);
 	let canBeCaluculated = "No";
-	let calucResultNumbers = new Set();
 
-	for (i = 1; i <= 9; i++)
-	{
-		for (j = 1; j <= 9; j++)
-		{
-			calucResultNumbers.add(i*j)
+	for (i = 1; i <= 9; i++) {
+		for (j = 1; j <= 9; j++) {
+			if (i * j === input) {
+				canBeCaluculated = "Yes";
+			} else {
+				break;
+			}
 		}
-	}
-
-	if (calucResultNumbers.has(input))
-	{
-		canBeCaluculated = "Yes";
 	}
 
 	console.log(canBeCaluculated)
