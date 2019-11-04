c533901e509c5d51125c506849f0ae9bddfd7785
melty12 <komei.1220@gmail.com>
2019-11-04 19:12:24 +0900

    clean: refactor code

--- 81/main.js
+++ 81/main.js
@@ -1,21 +1,20 @@
 "use strict";
 
-function Main(input)
-{
-	input = parseInt(input);
-	let canBeCaluculated = "No";
+function Main(input) {
+  input = parseInt(input);
+  let canBeCaluculated = "No";
 
-	for (i = 1; i <= 9; i++) {
-		for (j = 1; j <= 9; j++) {
-			if (i * j === input) {
-				canBeCaluculated = "Yes";
-			} else {
-				break;
-			}
-		}
-	}
+  for (i = 1; i <= 9; i++) {
+    for (j = 1; j <= 9; j++) {
+      if (i * j === input) {
+        canBeCaluculated = "Yes";
+      } else {
+        break;
+      }
+    }
+  }
 
-	console.log(canBeCaluculated)
+  console.log(canBeCaluculated);
 }
 
 Main(process.argv[2]);
--- 9x9/main.js
+++ 9x9/main.js
@@ -1,19 +1,14 @@
 "use strict";
 
-function Main(input)
-{
+function Main(input) {
   input = input.split(" ");
-  let result = "-1"
+  let result = "-1";
 
-  if (
-    input[0] >= 1 && input[0] <= 9 &&
-    input[1] >= 1 && input[1] <= 9
-  )
-  {
+  if (input[0] >= 1 && input[0] <= 9 && input[1] >= 1 && input[1] <= 9) {
     result = input[0] * input[1];
   }
 
-  console.log(result)
+  console.log(result);
 }
 
 Main(process.argv[2]);
