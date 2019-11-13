8c02aa74c608403cb195c0d06c8c535e54ab94cc
melty12 <komei.1220@gmail.com>
2019-11-14 07:54:47 +0900

    fix: powersocket

--- PowerSocket/main.ts
+++ PowerSocket/main.ts
@@ -1,12 +1,11 @@
-let input = "8 8";
+let input = "4 10";
 
 const main = (lines: string) => {
   const data = lines.split(" ");
   let division = parseInt(data[0], 10) - 1;
   let targetValue = parseInt(data[1], 10) - 1;
-  let result = 0;
 
-  console.log(Math.floor((division - 1 + targetValue) / division));
+  console.log(Math.ceil(targetValue / division));
 };
 
 main(input);
