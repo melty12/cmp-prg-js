52ff2d16a15e706ec387cfba7cfc7ec3ea80281b
melty12 <komei.1220@gmail.com>
2019-11-13 11:36:28 +0900

    tweak

--- PowerSocket/main.ts
+++ PowerSocket/main.ts
@@ -2,11 +2,11 @@ let input = "8 8";
 
 const main = lines => {
   const data = lines.split(" ");
-  let division = parseInt(data[0], 10);
-  let targetValue = parseInt(data[1], 10);
+  let division = parseInt(data[0], 10) - 1;
+  let targetValue = parseInt(data[1], 10) - 1;
   let result = 0;
 
-  console.log(Math.floor((division - 2 + (targetValue - 1)) / (division - 1)));
+  console.log(Math.floor((division - 1 + targetValue) / division));
 };
 
 main(input);
