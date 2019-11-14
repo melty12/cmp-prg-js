52e0822c9d4164fd1d451e67f31334a3fa04f39a
melty12 <komei.1220@gmail.com>
2019-11-14 10:37:05 +0900

    fixed: improve check for access outside the  array

--- Buffet/main.ts
+++ Buffet/main.ts
@@ -3,16 +3,16 @@ let input2 = "4\n2 3 4 1\n13 5 8 24\n45 9 15";
 
 const main = (lines: string) => {
   const data = lines.split("\n");
-  const foodTypes = data[1].split(" ").map(str => parseInt(str, 10));
+  const foodTypes = data[1].split(" ").map(str => parseInt(str, 10) - 1);
   const satisfactions = data[2].split(" ").map(str => parseInt(str, 10));
   let specialRule = data[3].split(" ").map(str => parseInt(str, 10));
   let result = 0;
 
-  foodTypes.forEach((food, index) => {
-    result += satisfactions[food - 1];
+  foodTypes.forEach((_, i) => {
+    result += satisfactions[foodTypes[i]];
 
-    if (food - foodTypes[index - 1] === 1) {
-      result += specialRule[food - 2];
+    if (i >= 1 && foodTypes[i] - foodTypes[i - 1] === 1) {
+      result += specialRule[foodTypes[i - 1]];
     }
   });
 
