e4c89305baeeea890c4cb8ecc6f11df10c665d90
melty12 <komei.1220@gmail.com>
2019-11-16 07:42:56 +0900

    Change: to impl not require sorting

--- OneClue/main.ts
+++ OneClue/main.ts
@@ -1,23 +1,22 @@
-let input = "1 100";
+let input = "11";
 
 const main = (lines: string) => {
   const data = lines.split(" ");
   const stoneLength = parseInt(data[0], 10);
   const stoneCoord = parseInt(data[1], 10);
-  let stoneCoords = [stoneCoord];
+  let stoneCoords = [];
 
-  for (let i = 1; i < stoneLength; i++) {
-    stoneCoords.push(stoneCoord - i);
-    stoneCoords.push(stoneCoord + i);
+  let k = stoneLength; // 石の連続する長さ
+  let x = stoneCoord; // 石の置かれている座標
+
+  const min = x - (k - 1); // 最小値
+  const max = x + (k - 1); // 最大値
+
+  for (let i = min; i <= max; i++) {
+    stoneCoords.push(i);
   }
 
-  let result = stoneCoords.sort(conmpareFunc).join(" ");
-
-  console.log(result);
-};
-
-const conmpareFunc = (a: number, b: number) => {
-  return a - b;
+  console.log(stoneCoord);
 };
 
 main(input);
