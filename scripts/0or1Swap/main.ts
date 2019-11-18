4c78cfa316c6e1001c0123866e81b0c3a129b4e8
melty12 <komei.1220@gmail.com>
2019-11-18 11:49:01 +0900

    fix: 0or1Swap

--- scripts/0or1Swap/main.ts
+++ scripts/0or1Swap/main.ts
@@ -1,40 +1,28 @@
-let input = "5\n5 2 3 4 1";
+const input = "5\n5 2 3 4 1";
 
 const main = (lines: string) => {
   const data = lines.split("\n");
   const sequence = data[1].split(" ").map(str => parseInt(str, 10));
+  const order = getOrder(sequence);
+  let correctSize: boolean[] = [];
   let result = "NO";
 
-  if (checkOrder(sequence)) result = "YES";
-
-  let _sequence;
-
-  for (let i = 0; i < sequence.length; i++) {
-    for (let j = 1; j < sequence.length; j++) {
-      _sequence = sequence.slice();
-      const temp = _sequence[i];
-      _sequence[i] = _sequence[j];
-      _sequence[j] = temp;
-
-      if (checkOrder(_sequence)) {
-        result = "YES";
-        break;
-      }
+  sequence.forEach((number, i) => {
+    if (number !== order[i]) {
+      correctSize.push(true);
     }
+  });
+
+  if (correctSize.length <= 2) {
+    result = "YES";
   }
 
   console.log(result);
 };
 
-const checkOrder = (sequence: number[]) => {
+const getOrder = (sequence: number[]) => {
   const temp = sequence.slice();
-  temp.sort((a, b) => (a < b ? -1 : 1));
-
-  temp.forEach((value, index) => {
-    if (value !== sequence[index]) return false;
-  });
-
-  return true;
+  return temp.sort((a, b) => (a < b ? -1 : 1));
 };
 
 main(input);
