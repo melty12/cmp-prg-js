442b9b41cbd29a25290208b0ae1b9e149d10ef2e
melty12 <komei.1220@gmail.com>
2019-11-20 11:30:39 +0900

    wip: bite eating

--- scripts/BiteEating/main.ts
+++ scripts/BiteEating/main.ts
@@ -5,19 +5,19 @@ const main = (lines: string) => {
   const appleLength = parseInt(data[0], 10);
   const tastePoint = parseInt(data[1], 10);
   let tastePoints: number[] = [];
-  let resultArr = [];
-  let sum = 0;
+  let result = 0;
 
   for (let i = 1; i <= appleLength; i++) {
     tastePoints.push(Math.abs(i + tastePoint - 1));
   }
 
-  resultArr = tastePoints.filter(n => n != Math.min.apply(null, tastePoints));
+  result = tastePoints
+    .filter(n => n != Math.min.apply(null, tastePoints))
+    .reduce((prev: number, current: number) => {
+      prev + current;
+    });
 
-  for (let i = 0; i < resultArr.length; i++) {
-    sum += resultArr[i];
-  }
-  console.log(sum);
+  console.log(result);
 };
 
 main(input);
