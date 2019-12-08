e44cac9bb2f154954c9e8910ae73dbf8fb8e4e08
melty12 <komei.1220@gmail.com>
2019-12-08 22:43:54 +0900

    fix: resale

--- scripts/Resale/main.ts
+++ scripts/Resale/main.ts
@@ -1,35 +1,16 @@
-let input = '1\n1\n50';
+let input = '3\n10 2 5\n6 3 4';
 
 const main = (input: string) => {
   const lines = input.trim().split("\n").slice(1)
   const V = lines[0].split(" ").map(str => parseInt(str))
   const C = lines[1].split(" ").map(str => parseInt(str))
-  const N = V.length;
-  let sumOfV = V.reduce((a, x) => a += x, 0)
-  let sumOfC = C.reduce((a, x) => a += x, 0)
-  let X: number[] = [];
-  let Y: number[] = [];
-  let result: number[] = [];
-  let resultMax;
-  let answer;
+  let answer = 0;
 
-  for (let i = 0; i < N; i++) {
-    X.push(V[i])
-    Y.push(C[i])
-    for (let j = i + 1; j < N; j++) {
-      X.push(V[i] + V[j])
-      Y.push(C[i] + C[j])
+  for (let i = 0; i < V.length; i++) {
+    if (V[i] > C[i]) {
+      answer += V[i] - C[i];
     }
   }
-  X.push(sumOfV);
-  Y.push(sumOfC);
-
-  for (let i = 0; i < X.length; i++) {
-    result.push(X[i] - Y[i])
-  }
-
-  resultMax = Math.max.apply(null, result);
-  answer = resultMax > 0 ? resultMax : 0
 
   console.log(answer);
 }
