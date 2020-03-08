911f03b137c1a6603b309086a439d9912ed366c7
komei otake <komei.1220@gmail.com>
2020-03-08 21:25:06 +0900

    fix faster divisor enumeration

--- scripts/K-thCommonDivisor/main.ts
+++ scripts/K-thCommonDivisor/main.ts
@@ -2,11 +2,31 @@ const main = (input: string) => {
   const [A, B, K] = input.split(" ").map(str => parseInt(str))
   let divisorList = [];
 
-  for (let i = A; i > 0; i--) {
-    if(A % i === 0 && B % i === 0) {
+  const calcGcd = (a: number, b: number): number => {
+    const r = a % b;
+    if(r === 0) {
+      return b;
+    }
+    return calcGcd(b, a % b)
+  }
+
+  const compareFunc = (a: number, b: number) => {
+    return b - a;
+  }
+
+  const gcd = calcGcd(A, B)
+  const squareRoot = Math.ceil(Math.sqrt(gcd))
+
+  for (let i = squareRoot; i > 0; i--) {
+    if(gcd % i === 0) {
       divisorList.push(i)
+      if(i !== gcd / i) {
+        divisorList.push(gcd / i)
+      }
     }
   }
+
+  divisorList.sort(compareFunc)
   console.log(divisorList[K - 1])
 }
 
