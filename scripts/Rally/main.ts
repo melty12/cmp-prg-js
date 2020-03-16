480139d0090e8cb440f45022a905648c03a7176a
melty12 <komei.1220@gmail.com>
2020-03-16 17:53:55 +0900

    wip Rally

--- scripts/Rally/main.ts
+++ scripts/Rally/main.ts
@@ -2,7 +2,18 @@ const main = (input: string) => {
   const lines = input.trim().split("\n")
   const N = parseInt(lines[0])
   const X = lines[1].split(" ").map(str => parseInt(str))
-  console.log(N, X);
+  const maxPoint = Math.max(...X)
+  let pointList: number[] = []
+
+  X.forEach((x, i) => {
+    for (let i = 1; i <= maxPoint; i++) {
+      if(i === N) {
+        pointList.push(Math.pow(x - i, 2))
+      }
+    }
+  });
+
+  console.log(pointList)
 }
 
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
