f82ed9cb4a947193a0090382ea31028b23dac452
melty12 <komei.1220@gmail.com>
2020-03-20 15:43:10 +0900

    remove conditional branch

--- scripts/FennecVsMonster/main.ts
+++ scripts/FennecVsMonster/main.ts
@@ -2,15 +2,13 @@ const main = (input: string) => {
   const lines = input.trim().split("\n")
   const [N, K] = lines[0].split(" ").map(str => parseInt(str))
   let H = lines[1].split(" ").map(str => parseInt(str))
-  H.sort((a, b) => a - b)
-
-  if(K > 0) { H.splice(-K) }
+  const removedMaxNumList = H.sort((a, b) => b - a).slice(K)
 
   const sum = (arr: number[]) => {
     return arr.reduce((a:number , b: number) => a + b, 0)
   }
 
-  console.log(sum(H))
+  console.log(sum(removedMaxNumList))
 }
 
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
