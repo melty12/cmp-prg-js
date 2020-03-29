f7a28bfccc024aa2169798e868a406548aa40ce7
melty12 <komei.1220@gmail.com>
2020-03-29 11:02:36 +0900

    fix HonestOrUnkind2

--- scripts/HonestOrUnkind2/main.ts
+++ scripts/HonestOrUnkind2/main.ts
@@ -1,21 +1,34 @@
 const main = (input: string) => {
-  const lines = input.trim().split("\n")
-  const testimonyList = new Map()
-  const A = lines.slice(1)
-  const X = A.filter(a => a.length !== 1).map(a => {
-    return a.split(" ").map(str => parseInt(str))
-  })
-
-  X.forEach(x => {
-    let testimony = x[1]
-    if(testimonyList.get(x[0])) {
-      testimony = testimonyList.get(x[0]) + x[1]
-    }
-    testimonyList.set(x[0], testimony)
-  })
-
-  console.log(testimonyList)
-
+  const lines: string[] = input.trim().split("\n");
+  const N = parseInt(lines[0]);
+  const testimonyList: number[][][] = [];
+  let i = 1;
+  while (i < lines.length - 1) {
+      const A = parseInt(lines[i++]);
+      const testimony: number[][] = Array(A).fill(0).map(() =>
+          lines[i++].split(' ').map(str => parseInt(str)))
+      testimonyList.push(testimony);
+  }
+​
+  let maxHonestNum = 0;
+  for (let bit = 0; bit < (1 << N); bit++) {
+      const isHonest: number[] = Array(N).fill(0);
+      for (let i = 0; i < N; i++) {
+          if (bit & (1 << i)) {
+              isHonest[i] = 1;
+          }
+      }
+      const isConsistent: boolean = testimonyList.every((testimony, i) =>
+          isHonest[i] ? testimony.every(xy => isHonest[xy[0] - 1] === xy[1]) : true
+      )
+​
+      if (isConsistent) {
+          const honestNum = isHonest.filter(b => b === 1).length;
+          maxHonestNum = Math.max(maxHonestNum, honestNum);
+      }
+  }
+​
+  console.log(maxHonestNum);
 }
-
+​
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
