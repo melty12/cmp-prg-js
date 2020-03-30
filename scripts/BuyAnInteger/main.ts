5d03bb42f9d1c54e0cad18a8dcdd9a569f604ff4
melty12 <komei.1220@gmail.com>
2020-03-30 16:36:30 +0900

    wip BuyAnInteger

--- scripts/BuyAnInteger/main.ts
+++ scripts/BuyAnInteger/main.ts
@@ -1,13 +1,27 @@
 const main = (input: string) => {
   const [A, B, X] = input.trim().split(" ").map(str => parseInt(str))
   let answer = 0
+  let leftNode = 0
+  let rightNode = Math.pow(10, 9) + 1
+  let count = 1
 
-  for (let i = 1; i < X; i++) {
-    if(A * i + B * String(i).length > X) {
-      answer = i - 1
+  while (rightNode - leftNode > 1) {
+    let center = Math.round((leftNode + rightNode) / 2)
+    let purchasableAmount = A * count + B * String(count).length
+
+    if(center === X) {
+      answer = center
       break
+    } else if (center < X){
+      leftNode = center + 1
+    } else {
+      rightNode = center - 1
     }
+
+    answer = center
+    count += 1
   }
+
   console.log(answer)
 }
 
