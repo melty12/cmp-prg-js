5b96eedad59ab04149333f43b94d443145072609
melty12 <komei.1220@gmail.com>
2020-04-01 11:16:35 +0900

    wip ButAnInteger

--- scripts/BuyAnInteger/main.ts
+++ scripts/BuyAnInteger/main.ts
@@ -1,25 +1,20 @@
 const main = (input: string) => {
   const [A, B, X] = input.trim().split(" ").map(str => parseInt(str))
   let answer = 0
-  let leftNode = 0
-  let rightNode = Math.pow(10, 9) + 1
-  let count = 1
+  let left = 0
+  let right = Math.pow(10, 9) + 1
 
-  while (rightNode - leftNode > 1) {
-    let center = Math.round((leftNode + rightNode) / 2)
-    let purchasableAmount = A * count + B * String(count).length
+  while (right - left > 1) {
+    let center = Math.round((left + right) / 2)
+    let purchasableAmount = A * center + B * String(center).length
 
-    if(center === X) {
-      answer = center
-      break
-    } else if (center < X){
-      leftNode = center + 1
+    if (purchasableAmount > X){
+      right = center
     } else {
-      rightNode = center - 1
+      left = center
     }
 
     answer = center
-    count += 1
   }
 
   console.log(answer)
